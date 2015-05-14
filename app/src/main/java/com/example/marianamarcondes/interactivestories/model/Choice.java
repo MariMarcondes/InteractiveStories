f18511f6b7b3e839cfed32d8ad82a7ce8d99c6bf
MariMarcondes <mari.marcondes@gmail.com>
2015-05-14 16:29:25 -0300

    Interactive Stories
    An exercise made during Treehouse Android Development track

---
+++ app/src/main/java/com/example/marianamarcondes/interactivestories/model/Choice.java
@@ -0,0 +1,32 @@
+package com.example.marianamarcondes.interactivestories.model;
+
+import java.lang.ref.SoftReference;
+
+/**
+ * Created by MarianaMarcondes on 5/3/15.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
